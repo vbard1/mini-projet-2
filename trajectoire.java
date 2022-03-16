506049ff57e53833eb65b073208db7e609c6562a
kaslabarak702 <101200976+kaslabarak702@users.noreply.github.com>
2022-03-16 16:24:50 +0100

    Update trajectoire.java

--- trajectoire.java
+++ trajectoire.java
@@ -1,14 +1,22 @@
+import java.util.ArrayList;
+
 public class trajectoire {
     int arrowType;
     int windSpeed;
     int angle;
     int speed;
+    ArrayList[] paramTraj;
 
-    public Trajectoire(int arrowType, int angleInit, int speedInit, int windSpeed){
+    public trajectoire(int arrowType, int angleInit, int speedInit, int windSpeed) {
         this.arrowType = arrowType;
         this.angle = angleInit;
         this.speed = speedInit;
         this.windSpeed = windSpeed;
-        
+        paramTraj = new ArrayList[3];
+        paramTraj[1] = new ArrayList<Integer>(); // param x
+        paramTraj[2] = new ArrayList<Integer>(); // param y
+        paramTraj[3] = new ArrayList<Double>(); // angle de la flèche avec l'horizontale
+
+        // taille
     }
 }
