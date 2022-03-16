44d7fb96d8c1572b366f74f764c3eb87b82d06bc
kaslabarak702 <101200976+kaslabarak702@users.noreply.github.com>
2022-03-16 14:42:14 +0100

    le monstre

--- UI.java
+++ UI.java
@@ -2,4 +2,5 @@ public class UI {
     // on peut tricher et rajouter une barre de défillement et la méthode get qui va
     // bien avec : )
     // http://web.mit.edu/java_v1.0.2/www/javadoc/java.awt.Scrollbar.html
+    
 }
