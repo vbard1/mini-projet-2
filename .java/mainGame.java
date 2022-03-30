2b4ef5ebbabefb64f236dad1f0e369ee6aea6749
kaslabarak702 <101200976+kaslabarak702@users.noreply.github.com>
2022-03-30 13:45:49 +0200

    Update mainGame.java

--- .java/mainGame.java
+++ .java/mainGame.java
@@ -4,7 +4,7 @@ public class mainGame extends JFrame {
     // TODO ici on met les TODO (bugs, trucs à faire encore)
 
     public static void main(String[] args) {
-        // launch(/* param? */);
+        launch(/* param? */);
 
         // TESTS trajectoire
         /*
