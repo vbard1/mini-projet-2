f29e168bb4e732ff24e222b7e0e31d5c7d3a5ffd
Clara-Leo <83869802+Clara-Leo@users.noreply.github.com>
2022-03-30 14:10:55 +0200

    Update game.java

--- .java/game.java
+++ .java/game.java
@@ -7,11 +7,18 @@ public class game implements ActionListener {
     double masse;
     int difficulty; // distance de la cible et et force du vent // de 1 à 3
     UI window;
+    int roundNb;
+    Arrow arrow;
+    Target target;
 
     public game(UI menu) {
         window = menu;
         player = new player();
         window.startGame.addActionListener(this);
+        roundNb = 5 ;
+        target = new Target()
+        onGoingGame();
+        gameEnd();
         
         
     }
@@ -26,17 +33,37 @@ public class game implements ActionListener {
             window.dispose();
             window=new UI('g');
             window.menu.addActionListener(this);
+            window.shoot.addActionListener(this);
         }else if(e.getSource()==window.menu){
             window.setVisible(false);
             window.dispose();
             window =new UI('m');
             window.startGame.addActionListener(this);
+        }else if(e.getSource() == window.shoot){
+            if (roundNb >= 0){
+                roundNb --;
+                double angleInit = window.angle.getValue() ;
+                double speedInit = window.speed.getValue() ;
+                Arrow arrow = new Arrow(masse, x, y, angleInit, speedInit);
+            }
+        }
+            
+    }
+    //TODO décompte score (fait)
+    //TODO gestion tours du jeu
+        // récupérer infos et créer flèche quand "shoot" cliqué (fait)
+        //Ajouter au score du joueur si la cible est touchée (fait)
+        //remettre la fenètre à zéro à chaque tour  
+
+    public void onGoingGame(){
+        while (roundNb > 0){
+            if(arrow.collision(target)){
+                player.score ++;
+            }
         }
     }
-    //TODO décompte score ?
-    //TODO gestion tours du jeu
-        // récupérer infos et créer trajectoire quand "launch" cliqué
-        //Ajouter au score du joueur si la cible est touchée
-        //remettre la fenètre à zéro à chaque tour
+    public void gameEnd(){
+        
+    }
 
 }
