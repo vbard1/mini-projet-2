51a3814cc83a7c68a18498caad2730610f256156
Aaabdou <101267176+Aaabdou@users.noreply.github.com>
2022-04-05 19:09:46 +0200

    Update gameZone.java

--- .java/gameZone.java
+++ .java/gameZone.java
@@ -5,6 +5,9 @@ import java.awt.*;
 public class gameZone extends JPanel {
     int width;
     int height;
+    Graphics graph;
+    int dessin;
+    Trajectoire traj;
     public gameZone(){
 
     }
@@ -13,11 +16,28 @@ public class gameZone extends JPanel {
         height=h;
     }
     public void paint(Graphics g){
+        graph=g;
+        System.out.println("g "+g);
         int [] xPoints={10,30,30,50};
         int [] yPoints={height,height,height-60,height-60};
         g.fillPolygon(xPoints,yPoints,xPoints.length);
         
-        g.fillOval(200, 200, 50, 50);;
+        g.fillOval(200, 200, 50, 50);
+        if(dessin==1){
+            for(int i= 0;i<traj.paramTraj[0].size();i+=3){
+                g.fillOval((int)(traj.paramTraj[0].get(i)),(int)(traj.paramTraj[1].get(i)),3,3);      
+                System.out.println("i :"+i);
+            }
+            g.fillOval(500,500,50,50);
+        System.out.println("graph "+g);
+        }
         
+        
+        
+    }
+    public void preview(Trajectoire t){
+        dessin=1;
+        traj=t;
+
     }
 }
