927ea6863f2553f54bd157a6ede9590dd2febaae
Aaabdou <101267176+Aaabdou@users.noreply.github.com>
2022-04-06 15:47:44 +0200

    Update UI.java

--- .java/UI.java
+++ .java/UI.java
@@ -22,7 +22,6 @@ import javax.swing.*;
 import java.awt.Color;
 import javax.swing.ImageIcon;
 
-
 //TODO mettre en relatif aux dimensions de la fenetre
 //TODO @abdel pour résoudre les barres qui s'affichent pas https://stackoverflow.com/questions/16737767/scroll-bar-not-visible-in-jpanel
 
@@ -40,7 +39,7 @@ public class UI extends JFrame implements ActionListener {
         JLabel textType;
         JButton startGame;
         JButton preview;
-        JLabel image ;
+        JLabel image;
 
         JPanel settings;
         JScrollBar angle;
@@ -55,8 +54,8 @@ public class UI extends JFrame implements ActionListener {
         // attributs-parametres du menu
         //
         public UI(char type) {
-                //initialisation du timer
-                
+                // initialisation du timer
+
                 this.type = type;
                 // Acquisition de la taille de l'écran
                 Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -72,19 +71,20 @@ public class UI extends JFrame implements ActionListener {
                 background = new JPanel();
                 background.setSize(this.getWidth(), this.getHeight());
                 background.setLocation(this.getInsets().left, this.getInsets().top);
-                background.setBackground(new Color(250,255,224));   //COULEUR FOND
+                background.setBackground(new Color(250, 255, 224)); // COULEUR FOND
                 background.setLayout(null);
-                
-                
 
-                //initialisation du timer
-                //resizeTimer = new Timer(100, this);
+                // initialisation du timer
+                // resizeTimer = new Timer(100, this);
 
                 if (type == 'm') {// Affichage du menu
 
                         resizeTimer = new Timer(100, this);
                         resizeTimer.start();
+<<<<<<< HEAD
 
+=======
+>>>>>>> 33528a27fff4ef0f9bd2fced36b34c5f8c750f3d
 
                         // Définition titre
                         title = new JLabel("Tir à l'arc");
@@ -102,7 +102,7 @@ public class UI extends JFrame implements ActionListener {
 
                         // Définition texte "difficulté"
                         textDifficulty = new JLabel("Difficulté");
-                        
+
                         textDifficulty.setHorizontalAlignment(SwingConstants.CENTER);
                         textDifficulty.setLayout(null);
 
@@ -112,87 +112,84 @@ public class UI extends JFrame implements ActionListener {
                         difficulty.setSelectedIndex(0);
                         difficulty.setLayout(null);
 
-                        
-
                         // Bouton pour lancer la partie
                         startGame = new JButton("JOUER");
-                        startGame.setBackground(new Color(51,204,102));
+                        startGame.setBackground(new Color(51, 204, 102));
                         startGame.setForeground(Color.WHITE);
                         startGame.setBorder(BorderFactory.createLineBorder(Color.WHITE, 15));
-                        //startGame.setLayout();
-                        
+                        // startGame.setLayout();
+
                         // Ajout image de fond
-						//image = new JLabel(new ImageIcon("./Images/archery_menu.png"));
-						//image.setBounds(500,500,100,150);
-						
-						
-						
-						
-						
-			title.setFont(new Font("Comic sans MS", Font.BOLD,(int)((background.getHeight()+background.getWidth())/40)));
-            title.setSize((int) background.getWidth(), (int)background.getHeight()/8);
-            title.setLocation(0, (int) background.getHeight()/10);
-            
-            textUser.setFont(new Font("Comic sans MS", Font.PLAIN,(int)background.getWidth()/50));
-            textUser.setSize((int) background.getWidth(), (int)background.getHeight()/16);
-            textUser.setLocation(0, (int) background.getHeight()/3);           
-            
-            username.setFont(new Font("Comic sans", Font.PLAIN,(int)background.getWidth()/90));
-            username.setSize((int) background.getWidth()/10, (int)background.getHeight()/25);
-            username.setLocation((int) ((background.getWidth() / 2) - (username.getWidth() / 2)),
+                        // image = new JLabel(new ImageIcon("./Images/archery_menu.png"));
+                        // image.setBounds(500,500,100,150);
+
+                        title.setFont(new Font("Comic sans MS", Font.BOLD,
+                                        (int) ((background.getHeight() + background.getWidth()) / 40)));
+                        title.setSize((int) background.getWidth(), (int) background.getHeight() / 8);
+                        title.setLocation(0, (int) background.getHeight() / 10);
+
+                        textUser.setFont(new Font("Comic sans MS", Font.PLAIN, (int) background.getWidth() / 50));
+                        textUser.setSize((int) background.getWidth(), (int) background.getHeight() / 16);
+                        textUser.setLocation(0, (int) background.getHeight() / 3);
+
+                        username.setFont(new Font("Comic sans", Font.PLAIN, (int) background.getWidth() / 90));
+                        username.setSize((int) background.getWidth() / 10, (int) background.getHeight() / 25);
+                        username.setLocation((int) ((background.getWidth() / 2) - (username.getWidth() / 2)),
                                         textUser.getLocation().y + textUser.getHeight() + 10);
-                                        
-            textDifficulty.setFont(new Font("Comic sans MS", Font.PLAIN,(int)background.getWidth()/60));
-            textDifficulty.setSize((int) background.getWidth()/10, (int)background.getHeight()/25);
-            textDifficulty.setLocation((int) ((background.getWidth() / 2) - (textDifficulty.getWidth() / 2)),
+
+                        textDifficulty.setFont(new Font("Comic sans MS", Font.PLAIN, (int) background.getWidth() / 60));
+                        textDifficulty.setSize((int) background.getWidth() / 10, (int) background.getHeight() / 25);
+                        textDifficulty.setLocation(
+                                        (int) ((background.getWidth() / 2) - (textDifficulty.getWidth() / 2)),
                                         username.getLocation().y + username.getHeight() + 50);
-                                        
-            difficulty.setFont(new Font("Comic sans", Font.PLAIN,(int)background.getWidth()/90));
-            difficulty.setLocation((int) ((background.getWidth() / 2) - (difficulty.getWidth() / 2)),
-            textDifficulty.getLocation().y + textDifficulty.getHeight() + 10);
-            difficulty.setSize((int) background.getWidth()/11, (int)background.getHeight()/25);
-            
-            /*textType.setFont(new Font("Comic sans MS", Font.PLAIN,(int)background.getWidth()/60));
-            textType.setSize((int) background.getWidth()/5, (int)background.getHeight()/25);
-            textType.setLocation((int) ((background.getWidth() / 2) - (textType.getWidth() / 2)),
-										difficulty.getLocation().y + difficulty.getHeight() + 50);
-		    
-            arrowType.setFont(new Font("Comic sans", Font.PLAIN,(int)background.getWidth()/90));		    
-		    arrowType.setLocation((int) ((background.getWidth() / 2) - (arrowType.getWidth() / 2)),
-            textType.getLocation().y + textType.getHeight() + 10);
-            arrowType.setSize((int) background.getWidth()/11, (int)background.getHeight()/25);*/
-            
-            startGame.setFont(new Font("Comic sans MS", Font.BOLD,(int)background.getWidth()/60));
-            startGame.setSize((int) background.getWidth()/10, (int)background.getHeight()/10);
-            startGame.setLocation((int) ((background.getWidth() / 2) - (startGame.getWidth() / 2)),
-            difficulty.getLocation().y + difficulty.getHeight() + 100);
-                                        
-						
-						
-						
-						
-						
-						
+
+                        difficulty.setFont(new Font("Comic sans", Font.PLAIN, (int) background.getWidth() / 90));
+                        difficulty.setLocation((int) ((background.getWidth() / 2) - (difficulty.getWidth() / 2)),
+                                        textDifficulty.getLocation().y + textDifficulty.getHeight() + 10);
+                        difficulty.setSize((int) background.getWidth() / 11, (int) background.getHeight() / 25);
+
+                        /*
+                         * textType.setFont(new Font("Comic sans MS",
+                         * Font.PLAIN,(int)background.getWidth()/60));
+                         * textType.setSize((int) background.getWidth()/5,
+                         * (int)background.getHeight()/25);
+                         * textType.setLocation((int) ((background.getWidth() / 2) -
+                         * (textType.getWidth() / 2)),
+                         * difficulty.getLocation().y + difficulty.getHeight() + 50);
+                         * 
+                         * arrowType.setFont(new Font("Comic sans",
+                         * Font.PLAIN,(int)background.getWidth()/90));
+                         * arrowType.setLocation((int) ((background.getWidth() / 2) -
+                         * (arrowType.getWidth() / 2)),
+                         * textType.getLocation().y + textType.getHeight() + 10);
+                         * arrowType.setSize((int) background.getWidth()/11,
+                         * (int)background.getHeight()/25);
+                         */
+
+                        startGame.setFont(new Font("Comic sans MS", Font.BOLD, (int) background.getWidth() / 60));
+                        startGame.setSize((int) background.getWidth() / 10, (int) background.getHeight() / 10);
+                        startGame.setLocation((int) ((background.getWidth() / 2) - (startGame.getWidth() / 2)),
+                                        difficulty.getLocation().y + difficulty.getHeight() + 100);
 
                         // Ajout à background
                         background.add(username);
                         background.add(difficulty);
-                        //background.add(arrowType);
+                        // background.add(arrowType);
                         background.add(startGame);
                         background.add(textDifficulty);
-                        //background.add(textType);
+                        // background.add(textType);
                         background.add(textUser);
                         background.add(title);
-                        //background.add(image);
+                        // background.add(image);
 
                         // (éventuellement animation du joueur en attente)
                 } else if (type == 'g') {
 
                         // Panel contenant l'affichage du jeu
 
-                        gameZone=new gameZone(background.getWidth(),background.getHeight()-150);
-                        gameZone.setSize(gameZone.width,gameZone.height);
-                        gameZone.setLocation(0,0);
+                        gameZone = new gameZone(background.getWidth(), background.getHeight() - 150);
+                        gameZone.setSize(gameZone.width, gameZone.height);
+                        gameZone.setLocation(0, 0);
 
                         gameZone.repaint();
                         // Panel contenant les réglages pour la flèche
@@ -245,7 +242,7 @@ public class UI extends JFrame implements ActionListener {
                         menu = new JButton("Menu");
                         menu.setSize(100, 50);
                         menu.setLocation(20, 20);
-                        //menu.setLayout(null);
+                        // menu.setLayout(null);
 
                         // jlabel score
                         score = new JLabel("Score");
@@ -262,7 +259,7 @@ public class UI extends JFrame implements ActionListener {
                         // Définition texte "Type de flèche"
                         textType = new JLabel("Type de flèche");
                         textType.setHorizontalAlignment(SwingConstants.CENTER);
-                        textType.setFont(new Font("Times New Roman", Font.BOLD,(int)background.getWidth()/60));
+                        textType.setFont(new Font("Times New Roman", Font.BOLD, (int) background.getWidth() / 60));
                         textType.setLayout(null);
 
                         // Définition type de flèche
@@ -291,18 +288,19 @@ public class UI extends JFrame implements ActionListener {
                 this.add(background);
 
                 setVisible(true);
-                
+
         }
 
         // TODO inclure le type d
         @Override
         public void actionPerformed(ActionEvent e) {
-                if (e.getSource()==resizeTimer && type=='m'){
+                if (e.getSource() == resizeTimer && type == 'm') {
                         resize();
                 }
-                
-                //repaint();
+
+                // repaint();
         }
+<<<<<<< HEAD
         
         public void resize(){
 			background.setSize(this.getWidth()-this.getInsets().right-this.getInsets().left,this.getHeight()-this.getInsets().top-this.getInsets().bottom);
@@ -355,7 +353,64 @@ public class UI extends JFrame implements ActionListener {
         public void moveArrow(){
                 
         }
+=======
+>>>>>>> 33528a27fff4ef0f9bd2fced36b34c5f8c750f3d
 
+        public void resize() {
+                background.setSize(this.getWidth() - this.getInsets().right - this.getInsets().left,
+                                this.getHeight() - this.getInsets().top - this.getInsets().bottom);
 
+                title.setFont(new Font("Comic sans MS", Font.BOLD,
+                                (int) ((background.getHeight() + background.getWidth()) / 40)));
+                title.setSize((int) background.getWidth(), (int) background.getHeight() / 8);
+                title.setLocation(0, (int) background.getHeight() / 10);
+
+                textUser.setFont(new Font("Comic sans MS", Font.PLAIN, (int) background.getWidth() / 50));
+                textUser.setSize((int) background.getWidth(), (int) background.getHeight() / 16);
+                textUser.setLocation(0, (int) background.getHeight() / 3);
+
+                username.setFont(new Font("Comic sans", Font.PLAIN, (int) background.getWidth() / 90));
+                username.setSize((int) background.getWidth() / 10, (int) background.getHeight() / 25);
+                username.setLocation((int) ((background.getWidth() / 2) - (username.getWidth() / 2)),
+                                textUser.getLocation().y + textUser.getHeight() + 10);
+
+                textDifficulty.setFont(new Font("Comic sans MS", Font.PLAIN, (int) background.getWidth() / 60));
+                textDifficulty.setSize((int) background.getWidth() / 10, (int) background.getHeight() / 25);
+                textDifficulty.setLocation((int) ((background.getWidth() / 2) - (textDifficulty.getWidth() / 2)),
+                                username.getLocation().y + username.getHeight() + 50);
+
+                difficulty.setFont(new Font("Comic sans", Font.PLAIN, (int) background.getWidth() / 90));
+                difficulty.setLocation((int) ((background.getWidth() / 2) - (difficulty.getWidth() / 2)),
+                                textDifficulty.getLocation().y + textDifficulty.getHeight() + 10);
+                difficulty.setSize((int) background.getWidth() / 11, (int) background.getHeight() / 25);
+
+                /*
+                 * textType.setFont(new Font("Comic sans MS",
+                 * Font.PLAIN,(int)background.getWidth()/60));
+                 * textType.setSize((int) background.getWidth()/5,
+                 * (int)background.getHeight()/25);
+                 * textType.setLocation((int) ((background.getWidth() / 2) -
+                 * (textType.getWidth() / 2)),
+                 * difficulty.getLocation().y + difficulty.getHeight() + 50);
+                 * 
+                 * arrowType.setFont(new Font("Comic sans",
+                 * Font.PLAIN,(int)background.getWidth()/90));
+                 * arrowType.setLocation((int) ((background.getWidth() / 2) -
+                 * (arrowType.getWidth() / 2)),
+                 * textType.getLocation().y + textType.getHeight() + 10);
+                 * arrowType.setSize((int) background.getWidth()/11,
+                 * (int)background.getHeight()/25);
+                 */
+
+                startGame.setFont(new Font("Comic sans MS", Font.BOLD, (int) background.getWidth() / 60));
+                startGame.setSize((int) background.getWidth() / 10, (int) background.getHeight() / 10);
+                startGame.setLocation((int) ((background.getWidth() / 2) - (startGame.getWidth() / 2)),
+                                difficulty.getLocation().y + difficulty.getHeight() + 100);
+                startGame.setBorder(BorderFactory.createLineBorder(Color.WHITE, 15 / 2));
+
+                // image.setLocation(100,200);
+                // image.setSize(100,150);
+
+        }
 
 }
