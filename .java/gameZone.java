103766e48d9f4cbd7b8cf26dcd9fb8e1d56b27cb
Aaabdou <101267176+Aaabdou@users.noreply.github.com>
2022-04-06 14:17:07 +0200

    Update gameZone.java

--- .java/gameZone.java
+++ .java/gameZone.java
@@ -25,14 +25,15 @@ public class gameZone extends JPanel {
             g.fillRect(0,0,this.getWidth(),this.getHeight());
             g.setColor(Color.black);
             for(int i= 0;i<traj.paramTraj[0].size();i+=3){
-                g.fillOval((int)(traj.paramTraj[0].get(i)),(int)(traj.paramTraj[1].get(i)),3,3);      
-                System.out.println("i :"+i);
+                g.fillOval((int)(traj.paramTraj[0].get(i)),this.height-(int)(traj.paramTraj[1].get(i)),3,3);      
             }
         }
         int [] xPoints={10,30,30,50};
         int [] yPoints={height,height,height-60,height-60};
         g.fillPolygon(xPoints,yPoints,xPoints.length);
-        
+        g.setColor(Color.red);
+        g.drawRect(0,0,this.width,this.height);
+        g.setColor(new Color(250,255,224));
         g.fillOval(200, 200, 50, 50);
         
         
