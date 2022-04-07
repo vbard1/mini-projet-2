f6a43449b5bb2b4f33d2ca362aea6b3bfa8c3f0f
Aaabdou <101267176+Aaabdou@users.noreply.github.com>
2022-04-07 08:46:45 +0200

    Update gameZone.java

--- .java/gameZone.java
+++ .java/gameZone.java
@@ -1,13 +1,17 @@
 
 import javax.swing.*;
 import java.awt.*;
-
-public class gameZone extends JPanel {
+import java.awt.event.*;
+public class gameZone extends JPanel implements ActionListener {
     int width;
     int height;
     Graphics graph;
     int dessin;
     Trajectoire traj;
+    Arrow arrow;
+    Timer arrowTimer;
+    int i=0;
+
     public gameZone(){
 
     }
@@ -15,6 +19,7 @@ public class gameZone extends JPanel {
         width=w;
         height=h;
         repaint();
+        arrowTimer=new Timer(100,this);
         setVisible(true);
     }
     public void paint(Graphics g){
@@ -29,13 +34,20 @@ public class gameZone extends JPanel {
             }
         } else if(dessin==2){
 
+                g.setColor(new Color(250,255,224));
+                g.fillRect(0,0,this.getWidth(),this.getHeight());
+                g.setColor(arrow.arrowColor);
+                g.drawLine((int)((int)arrow.traj.paramTraj[0].get(i)-(arrow.length/2)*Math.cos((double)(arrow.traj.paramTraj[2].get(i)))),
+                this.height-((int)((int)arrow.traj.paramTraj[1].get(i)-(arrow.length/2)*Math.sin((double)(arrow.traj.paramTraj[2].get(i))))) , 
+                (int)((int)arrow.traj.paramTraj[0].get(i)+(arrow.length/2)*Math.cos((double)(arrow.traj.paramTraj[2].get(i)))), 
+                this.height-((int)((int)arrow.traj.paramTraj[1].get(i)+(arrow.length/2)*Math.sin((double)(arrow.traj.paramTraj[2].get(i))))));
+                g.drawString(""+i,10,10);
+                g.drawString(""+(double)(arrow.traj.paramTraj[2].get(i)),20,20);
+
         }
         int [] xPoints={10,30,30,50};
         int [] yPoints={height,height,height-60,height-60};
         g.fillPolygon(xPoints,yPoints,xPoints.length);
-        //g.setColor(Color.red);
-        //g.drawRect(0,0,this.width,this.height);
-        //g.setColor(new Color(250,255,224));
         g.fillOval(200, 200, 50, 50);
         
         
@@ -44,7 +56,24 @@ public class gameZone extends JPanel {
     public void preview(Trajectoire t){
         dessin=1;
         traj=t;
+        i=0;
         repaint();
         
+    }
+    public void shoot(Arrow a){
+        dessin=2;
+        arrowTimer.start();
+        arrow=a;
+        
+
+    }
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource()==arrowTimer && i<arrow.traj.paramTraj[0].size()){
+            i++;
+            repaint();
+        }else if(e.getSource()==arrowTimer && i>=arrow.traj.paramTraj[0].size()){
+            arrowTimer.stop();
+            i=0;
+        }
     }
 }
