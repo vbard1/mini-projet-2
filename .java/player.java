9410db11c2121dc03ac71cbb29ddd8526ba81ca7
Aaabdou <101267176+Aaabdou@users.noreply.github.com>
2022-04-21 08:34:40 +0000

    Update player.java

--- .java/player.java
+++ .java/player.java
@@ -1,17 +1,17 @@
-public class player {
+public class Player {
 
     // TODO highscore;
     public String name;
     public int score;
 
-    public player(String nm) {
+    public Player(String nm) {
         name = nm;
         score = 0;
     }
     // possible amelioration : reprendre la derniere partie : fichier sauvegarde +
     // score = prevScore (pas forcément dur)
 
-    public player() {
+    public Player() {
         name = "Anonymous unicorn";
         score = 0;
     }
