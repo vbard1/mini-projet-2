2dd10ebd2c11eaa25d83bd6db7bb1543797dc52f
Aaabdou <101267176+Aaabdou@users.noreply.github.com>
2022-05-01 18:55:28 +0200

    Update Target.java

--- .java/Target.java
+++ .java/Target.java
@@ -1,19 +1,19 @@
 public class Target {
 
     public int posX; // position sur x
-    public int posLowY; // bas de la cible
-    public int height; // haut de la cible
+    public int posY; // haut de la cible
+    public int side; // logueur côté de la cible
 
-    public Target(int posX, int height) { // placer une cible sur le sol
+    public Target(int posX, int side) { // placer une cible sur le sol
         this.posX = posX;
-        this.posLowY = 0;
-        this.height = height;
+        this.posY = 0;
+        this.side = side;
     }
 
-    public Target(int posX, int posLowY, int height) { // placer une cible en hauteur
+    public Target(int posX, int posY, int side) { // placer une cible en hauteur
         this.posX = posX;
-        this.posLowY = posLowY;
-        this.height = height;
+        this.posY = posY;
+        this.side = side;
     }
 
 }
