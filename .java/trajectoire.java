86bb4416546fd773c94030beedd6d2dca8b20792
kaslabarak702 <101200976+kaslabarak702@users.noreply.github.com>
2022-05-02 20:43:37 +0200

    Update trajectoire.java

--- .java/trajectoire.java
+++ .java/trajectoire.java
@@ -67,7 +67,7 @@ public class Trajectoire {
         int y = 0;
         double vitesse;
         int absciss = 0;
-        angleInitDeg /= (windSpeed + 1);
+        speedInit /= (windSpeed/2 + 1);
         while (absciss < maxX) {
             // Trajectoire de y en fonction de x dans le vide
             vitesse = Math.sqrt(speedInit * speedInit - 2 * gravity * absciss * Math.tan(angleInitDeg * degToRad)
@@ -76,7 +76,7 @@ public class Trajectoire {
             y = (int) (-0.5 * gravity / (speedInit * speedInit) * absciss * absciss *
                     (1 + Math.pow(Math.tan(angleInitDeg * degToRad), 2))
                     + absciss * Math.tan(angleInitDeg * degToRad) + yInit);
-            //System.out.print(y + ";");
+            // System.out.print(y + ";");
 
             if (y > -1) {
                 // System.out.print( "\n Windspeed : " + windSpeed + " => I retrieve to y " +
@@ -123,7 +123,7 @@ public class Trajectoire {
     }
 
     public String toString() {
-        //System.out.println("EXCEL VERSION : nValues = " + paramTraj[0].size());
+        // System.out.println("EXCEL VERSION : nValues = " + paramTraj[0].size());
         String Stringed = "";
         for (int i = 0; i < paramTraj[0].size() - 1; i++) {
 
