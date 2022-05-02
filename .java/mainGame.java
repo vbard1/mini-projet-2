94f5088b8bc05ef5672b0809c52775a4a8644f6e
kaslabarak702 <101200976+kaslabarak702@users.noreply.github.com>
2022-05-02 20:34:41 +0200

    cleaning p1.

--- .java/mainGame.java
+++ .java/mainGame.java
@@ -1,41 +1,11 @@
 import javax.swing.JFrame;
 
 public class MainGame extends JFrame {
-    // TODO ici on met les TODO (bugs, trucs à faire encore)
-
     public static void main(String[] args) {
-        // launch(/* param? */);
-        launch(/* param? */);
-        /*
-         * Trajectoire e = new Trajectoire(45, 30, 1, 10, 10);
-         * System.out.println(" 1 \n" + e.toString());
-         * e.recalculate(45, 30, 2, 10, 10);
-         * System.out.println(" 2\n" + e.toString());
-         * e.recalculate(45, 30, 0, 10, 10);
-         * System.out.println(" 0\n" + e.toString());
-         */
-
-        // TESTS trajectoire
-
-        // Test de performance
-        /*
-         * long avant = System.currentTimeMillis();
-         * int nCalc = 6;
-         * Trajectoire e = new Trajectoire(45, 150, 1, 0);
-         * for (int i = 0; i < nCalc - 1; i++) {
-         * e.recalculate(45, 150, 1, 0);
-         * }
-         * int size = e.paramTraj[0].size();
-         * System.out.println("temps de calcul de trajectoire = " +
-         * (System.currentTimeMillis() - avant) / (size * nCalc)
-         * + " ms/point, or " + (System.currentTimeMillis() - avant) / (nCalc) +
-         * "ms per trajectory => maxX = "
-         * + e.paramTraj[0].size());
-         */
-
+        launch();
     }
 
-    public static void launch(/* param? */) {
+    public static void launch() {
         Game newGame = new Game(new UI('m'));
     }
 }
